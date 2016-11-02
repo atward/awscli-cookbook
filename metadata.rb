f71663eb14e1a739243387e0bd86bbd3d087d8cf
Adam Ward <adamw@subdesigns.net>
2016-11-02 15:13:08 +1100

    Version bump: 1.1.3
    To differentiate due to proxy patch

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ndowns@amazon.com'
 license 'Apache 2.0'
 description 'Defines a number of LWRP wrapper commands around the awscli command line script'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.1.1'
+version '1.1.3'
 source_url 'https://github.com/awslabs/awscli-cookbook' if respond_to?(:source_url)
 issues_url 'https://github.com/awslabs/awscli-cookbook/issues' if respond_to?(:issues_url)
 
